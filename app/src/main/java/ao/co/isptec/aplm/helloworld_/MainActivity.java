0d177ece918ea0a70e4ca0cb2ea30934c6906e4a
hamil <20211961@isptec.co.ao>
2024-10-08 11:09:31 +0100

    First Commit

---
+++ app/src/main/java/ao/co/isptec/aplm/helloworld_/MainActivity.java
@@ -0,0 +1,42 @@
+package ao.co.isptec.aplm.helloworld_;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "ao.co.isptec.aplm.helloworld.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Minha mensagem de debug!");
+
+
+        Button send_btn_ = (Button) findViewById(R.id.send_btn);
+        send_btn_.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+                EditText editText = (EditText) findViewById(R.id.edit_message);
+                String message = editText.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
